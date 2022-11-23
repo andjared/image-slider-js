beb4e8f1e286d0fcf2b5f79299b93fbd07d2121d
andja <andja.velimirovic@gmail.com>
2022-11-23 07:39:28 +0100

    slide order

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ const nextBtn = document.querySelector("#next-btn");
 
 const slides = document.querySelectorAll(".slide");
 
+const order = document.querySelector(".slide-order");
+
 slides.forEach((slide, index) => {
-  console.log(index);
   slide.style.left = `${index * 100}%`;
 });
 
 let counter = 0;
-// console.log(counter);
 
-const slider = (type) => {
-  console.log(counter);
+const slider = () => {
+  if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+
+  order.textContent = `${counter + 1} / 6`;
 
   slides.forEach(
     (slide) => (slide.style.transform = `translateX(-${counter * 100}%)`)
